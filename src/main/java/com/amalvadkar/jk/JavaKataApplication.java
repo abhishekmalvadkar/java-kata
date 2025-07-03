726de89351e990c811254294457c72af7b7bc6fb
Abhishek Malvadkar <abhi@dev.com>
2025-07-03 14:26:44 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/jk/JavaKataApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/jk/JavaKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
