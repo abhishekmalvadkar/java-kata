299968bdb7187c87eb0b29b00c91ef7a3e478cea
Abhishek Malvadkar <abhi@dev.com>
2025-07-04 11:29:42 +0530

    GH-2 : Testing Time-Based Logic Using Clock kata

---
+++ src/main/java/com/amalvadkar/jk/datetime/GreetingService.java
@@ -0,0 +1,20 @@
+package com.amalvadkar.jk.datetime;
+
+import lombok.RequiredArgsConstructor;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+public class GreetingService {
+
+    private final Clock clock;
+
+    public String greet() {
+        LocalDateTime currentTime = LocalDateTime.now(clock);
+        int currentHour = currentTime.getHour();
+        if (currentHour < 12) return "Good Morning";
+        if (currentHour < 18) return "Good Afternoon";
+        return "Good Evening";
+    }
+}
---
+++ src/test/java/com/amalvadkar/jk/datetime/GreetingServiceTest.java
@@ -0,0 +1,43 @@
+package com.amalvadkar.jk.datetime;
+
+import com.amalvadkar.jk.common.AbstractUT;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GreetingServiceTest extends AbstractUT {
+
+    @Test
+    void should_return_good_morning() {
+        Clock fixedClock = Clock.fixed(
+                LocalDateTime.of(2025, 7, 4, 9, 0).atZone(ZoneId.systemDefault()).toInstant(),
+                ZoneId.systemDefault()
+        );
+        GreetingService greetingService = new GreetingService(fixedClock);
+        assertThat(greetingService.greet()).isEqualTo("Good Morning");
+    }
+
+    @Test
+    void should_return_good_afternoon() {
+        Clock fixedClock = Clock.fixed(
+                LocalDateTime.of(2025, 7, 4, 13, 0).atZone(ZoneId.systemDefault()).toInstant(),
+                ZoneId.systemDefault()
+        );
+        GreetingService greetingService = new GreetingService(fixedClock);
+        assertThat(greetingService.greet()).isEqualTo("Good Afternoon");
+    }
+
+    @Test
+    void should_return_good_evening() {
+        Clock fixedClock = Clock.fixed(
+                LocalDateTime.of(2025, 7, 4, 19, 0).atZone(ZoneId.systemDefault()).toInstant(),
+                ZoneId.systemDefault()
+        );
+        GreetingService greetingService = new GreetingService(fixedClock);
+        assertThat(greetingService.greet()).isEqualTo("Good Evening");
+    }
+}
